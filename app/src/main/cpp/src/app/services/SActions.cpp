6452bf6f24e5d14a9cd20f970b9f227c215c9374
Aslan <moctodliamgsc@gmail.com>
2024-11-23 20:38:00 +0000

    Use port from argument; declare SActions::process, SActions::sendActions; Use addRoute, addWsRoute

--- app/src/main/cpp/src/app/services/SActions.cpp
+++ app/src/main/cpp/src/app/services/SActions.cpp
@@ -1,5 +1,6 @@
 #include <utility>
-
+#include <functional>
+#include <thread>
 #include <crow.h>
 
 #include "../action/action.h"
@@ -11,45 +12,23 @@
  */
 SActions::SActions(unsigned int port)
 {
-    application(8080);
-    CROW_ROUTE(application, "/actions")
-    ([](const crow::request& request,
-            crow::response& response) {
-        NAction::TAction action(std::move(request));
-        if (action.empty()) {
-            response.code = 400;
-            response.body = "Bad JSON";
-            response.end();
-            return;
+    application(port);
+
+    application.addRoute(
+        "/action",
+        crow::HTTPMethod::POST,
+        process
+    );
+
+    application.addWsRoute(
+        "/ws",
+        [](crow::websocket::connection& connection) {
+            std::thread(
+                &sendActions,
+                std::ref(connection)
+            ).detach();
         }
-        response.code = 200;
-        response.body = "Received an action";
-        response.end();
-        return;
-    });
-
-    CROW_WEBSOCKET_ROUTE(application, "/ws")
-        .onopen([&](crow::websocket::connection& connection) {
-
-                })
-        .onclose([&](crow::websocket::connection& connection,
-                    const std::string& reason,
-                    uint16_t code) {
-
-                })
-        .onaccept([&](const crow::request& request,
-                    void** data) {
-                return true/false;
-                })
-        .onmessage([&](crow::websocket::connection& connection,
-                    const std::string& message,
-                    bool isBinary) {
-
-                })
-        .onerror([&](crow::websocket::connection& connection,
-                    const std::string& error) {
-
-                });
+    );
 }
 
 /**
@@ -57,6 +36,48 @@ SActions::SActions(unsigned int port)
  */
 void SActions::run() {
     application.run();
+}
+
+/**
+ * @brief Process requested actions
+ * @param request Request to parse to an action
+ * @param response Response to fill and send to a client
+ */
+void SActions::process(const crow::request& request,
+        crow::response& response) {
+    NAction::TAction action(std::move(request));
+    if (action.empty()) {
+        response.code = crow::status::BAD_REQUEST;
+        response.body = "Bad JSON";
+        response.end();
+        return;
+    }
+    queue->push(
+        std::make_unique
+            <NAction::TAction>(action)
+    );
+    response.code = crow::status::OK;
+    response.body = "Received an action";
+    response.end();
+    return;
+}
+
+/**
+ * @brief Send actions in
+ * JSON format to a client
+ * @param connection Connection with client
+ */
+void SActions::sendActions(crow::websocket::connection& connection) {
+    while (!shutdownFlag) {
+        auto action = queue->pop();
+        if (action == nullptr) {
+            continue;
+        }
+        connection.send_text(
+            action
+                ->json().dump(4)
+        );
+    }
 }
 
 /**
--- app/src/main/cpp/src/app/services/SActions.h
+++ app/src/main/cpp/src/app/services/SActions.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include <memory>
+#include <crow.h>
 
 #include "../action/action.h"
 #include "../requests/manual.h"
 #include "../../base/IService.h"
 #include "../../base/Queue.h"
 
+/**
+ * @brief Service to manage client actions:
+ * - `/action`: API to receive an action
+ * - `/ws`: WebSocket connection to see
+ * received actions in real time
+ */
 class SActions : public IService {
   public:
     SActions(unsigned int port);
@@ -14,6 +21,9 @@ class SActions : public IService {
     void run() override;
     void shutdown() override;
   private:
+    void process(const crow::request& request,
+        crow::response& response);
+    void sendActions(crow::websocket::connection& connection);
     /**
      * @brief Application to receive actions
      */
