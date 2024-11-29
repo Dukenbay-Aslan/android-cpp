63102260e120edae01d5267407535b4ca893ce82
Aslan <moctodliamgsc@gmail.com>
2024-11-29 21:44:23 +0000

    Run SActions. Catch signals

--- app/src/main/cpp/src/Main.cpp
+++ app/src/main/cpp/src/Main.cpp
@@ -1,24 +1,125 @@
+#include <signal.h>
+#include <chrono>
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <memory>
+#include <utility>
+#include <thread>
+#include <vector>
 #include <iostream>
 
+#include "app/services/SActions.h"
 #include "base/config/Config.h"
 #include "base/logger/logger.h"
-#include "base/Queue.h"
+#include "base/IService.h"
+
+/**
+ * @brief 24 hours
+ */
+const auto oneDay = std::chrono::hours(24);
+/**
+ * @brief Flag to signal
+ * all services to shut down
+ */
+std::atomic<bool> shutdownFlag = false;
+/**
+ * @brief Mutual exclusion for
+ * `shutdownCv`
+ */
+std::mutex shutdownMutex;
+/**
+ * @brief Condition variable to
+ * control shut down of services
+ */
+std::condition_variable shutdownCv;
+
+/**
+ * @brief Catch signals and
+ * notify services to shut down
+ * @param sig Signal received
+ */
+void signalHandler(int sig) {
+    shutdownFlag = true;
+}
 
 int main() {
-    TQueue<int> q;
-    q.push(1);
-    auto x = q.pop();
-    std::cout << x << std::endl;
-
-    if (!Config::parse("config.json")) {
-        return -1;
-    }
-
     /**
      * @brief Messages logger
      */
     TLogger log("Main");
-    log.info << "Successfully parsed configurations: " <<
-        Config::json().dump(4) << '\n';
+    log.info
+        << "Parsing config.json"
+        << std::endl;
+    if (!Config::parse("config.json")) {
+        log.error
+            << "Error in parsing configurations. "
+            << std::endl;
+        return -1;
+    }
+    log.info
+        << "Successfully parsed configurations: "
+        << Config::json().dump(4)
+        << std::endl;
+    
+    // Catching signals
+    signal(
+        SIGINT,
+        signalHandler
+    );
+    signal(
+        SIGTERM,
+        signalHandler
+    );
+    signal(
+        SIGABRT,
+        signalHandler
+    );
+
+    std::vector<std::shared_ptr<IService>> services;
+    auto actionsService = std::make_shared<SActions>(
+        8080
+    );
+
+    /**
+     * @brief `run` functions of services
+     */
+    std::vector<std::thread> threads;
+    for (auto& service : services) {
+        threads.push_back(std::thread(&IService::run, service));
+    }
+
+    /**
+     * Blocking services here until
+     * SIGINT received and
+     * shutdownFlag set true.
+     * Checking this condition
+     * every 24 hours starting
+     * from program start
+     */
+    {
+        /**
+         * @brief Lock mutex for condition variable
+         */
+        std::unique_lock<std::mutex> lock(shutdownMutex);
+        shutdownCv.wait_for(
+            lock,
+            oneDay,
+            shutdownFlag.load()
+        );
+        lock.unlock();
+        shutdownCv.notify_all();
+    }
+
+    for (auto& thread : threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+
+    log.info
+        << "Joined all threads"
+        << std::endl;
+
     return 0;
 }
