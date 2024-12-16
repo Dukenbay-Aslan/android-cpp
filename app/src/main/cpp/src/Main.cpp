73a1e498c91842c3e8f73ded1221e9a4d8bfaa2c
Aslan <moctodliamgsc@gmail.com>
2024-12-16 00:20:52 +0000

    Make main log general

--- app/src/main/cpp/src/Main.cpp
+++ app/src/main/cpp/src/Main.cpp
@@ -36,6 +36,11 @@ std::mutex shutdownMutex;
 std::condition_variable shutdownCv;
 
 /**
+ * @brief Messages logger
+ */
+TLogger log("Main");
+
+/**
  * @brief Catch signals and
  * notify services to shut down
  * @param sig Signal received
@@ -46,10 +51,6 @@ void signalHandler(int sig) {
 }
 
 int main(int argc, char *argv[]) {
-    /**
-     * @brief Messages logger
-     */
-    TLogger log("Main");
     if (argc != 2) {
         log.error << "Give a path to configuration file";
         return -1;
